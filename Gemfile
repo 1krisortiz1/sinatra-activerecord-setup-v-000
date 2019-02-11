cd07879cf12a256cda86aca5960717634fb57346
Kristin Ortiz <itskristin.ortiz@gmail.com>
2019-02-11 18:34:47 +0000

    Automatically backed up by Learn

--- Gemfile
+++ Gemfile
@@ -13,7 +13,11 @@ group :development do
 	gem 'shotgun'
 	gem 'pry'
     gem 'tux'
+<<<<<<< HEAD
     gem 'sqlite3', '~> 1.3.6'
+=======
+    gem 'sqlite3'
+>>>>>>> e9650019e7f70f3903ca4540496aaf89331332e9
 end
 
 group :test do
--- config/environment.rb
+++ config/environment.rb
@@ -3,8 +3,13 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
+<<<<<<< HEAD
 configure :development do
   set :database, 'sqlite3:db/database.db'
+=======
+configure :development do #this sets up a connection to a sqlite3 database named db/database.db located in a folder called "db"
+    set :database, 'sqlite3:db/database.db'
+>>>>>>> e9650019e7f70f3903ca4540496aaf89331332e9
 end
 
 require './app'
